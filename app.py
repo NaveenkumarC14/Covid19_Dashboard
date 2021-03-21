db0cf2267bfc47f989f3ae3c2eb45f414be09eab
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-03-21 23:16:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,9 +104,9 @@ Total_Active=total['Number of cases'].iloc[1]
 st.markdown('''
 
     <div class="col-sm-4">
-      <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: red'>Total Confirmed</p>
-      <p style='text-align:center; font-size: 15px; color: red'>[''' + str(Total_Confirmed) + ''']</p>
-      <p style='text-align: center; font-size: 35px; font-weight: bold; color: red'>''' + str(Total_Confirmed) + '''</p>
+      <p style='text-align: margin-left; background-color: #fff; font-weight: 400 ;color: red'>Total Confirmed</p>
+      <p style='text-align:margin-left; font-size: 15px; color: red'>[''' + str(Total_Confirmed) + ''']</p>
+      <p style='text-align: margin-left; font-size: 35px; font-weight: bold; color: red'>''' + str(Total_Confirmed) + '''</p>
     </div>
     <div class="col-sm-4" style='background-color: #fff; border-radius: 5px'>
       <p style='text-align:center; font-weight: 400 ; color: #000'>Total Deaths</p>
