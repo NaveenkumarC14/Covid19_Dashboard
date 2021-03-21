5ca1da6694dbd767beebaa2100cd796df7e12a18
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-03-21 17:22:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -106,17 +106,17 @@ st.markdown('''
   <div class="row" style="background-color: #fff;width: 100%; margin: auto;">
     <div class="col-sm-4">
       <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: red'>Total Confirmed</p>
-      <p style='text-align:center; font-size: 15px; color: red'>[''' + str(Total_Confirmed) + ''']</p>
+      //<p style='text-align:center; font-size: 15px; color: red'>[''' + str(Total_Confirmed) + ''']</p>
       <p style='text-align: center; font-size: 35px; font-weight: bold; color: red'>''' + str(Total_Confirmed) + '''</p>
     </div>
     <div class="col-sm-4" style='background-color: #fff; border-radius: 5px'>
       <p style='text-align:center; font-weight: 400 ; color: #000'>Total Deaths</p>
-      <p style='text-align: center; font-size: 15px; color: #e73631'>[''' + str(Total_Death) + ''']</p>
+      //<p style='text-align: center; font-size: 15px; color: #e73631'>[''' + str(Total_Death) + ''']</p>
       <p style='text-align: center; font-size: 35px; font-weight: bold; color: #e73631'>''' + str(Total_Death) + '''</p>
     </div>
     <div class="col-sm-4">
       <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: #000'>Total Recovered</p>
-      <p style='text-align: center; font-size: 15px; color: #70a82c'>[''' + str(Total_Recovered) + ''']</p>
+      //<p style='text-align: center; font-size: 15px; color: #70a82c'>[''' + str(Total_Recovered) + ''']</p>
       <p style='text-align: center; font-size: 35px; font-weight: bold; color: #70a82c'>''' + str(Total_Recovered) + '''</p>
     </div>
  ''', unsafe_allow_html=True);
