ccff2de096db16c39aca0c98c7ae6f4bc9b595f4
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-03-22 08:20:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -103,6 +103,7 @@ Total_Recovered=total['Number of cases'].iloc[2]
 Total_Active=total['Number of cases'].iloc[1]
 st.markdown('''
 <div class="jumbotron text-center" style='padding: 0px'>
+ </div>
  <div class="row" style="background-color:#fff;width:100%;margin:auto;">
     <div class="col-sm-4">
       <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: red'>Total Confirmed</p>
@@ -122,7 +123,7 @@ st.markdown('''
     
     </div>
  </div>
-</div>
+
  ''', unsafe_allow_html=True);
     
 def get_table():
