2bf2367542d9248902958394a12bd90439234aff
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-03-22 08:12:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -102,15 +102,13 @@ Total_Death=total['Number of cases'].iloc[3]
 Total_Recovered=total['Number of cases'].iloc[2]
 Total_Active=total['Number of cases'].iloc[1]
 st.markdown('''
-    
+   <div class="row" style="background-color:#fff;width:100%;margin:auto;">
     <div class="col-sm-4">
-    <style>
-      col-sm-4{display:inline-block}
-    </style>
+      
         
-      <p style='text-align: margin-left; background-color: #fff; font-weight: 400 ;color: red'>Total Confirmed</p>
-      <p style='text-align:margin-left; font-size: 15px; color: red'>[''' + str(Total_Confirmed) + ''']</p>
-      <p style='text-align: margin-left; font-size: 35px; font-weight: bold; color: red'>''' + str(Total_Confirmed) + '''</p>
+      <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: red'>Total Confirmed</p>
+      <p style='text-align:center; font-size: 15px; color: red'>[''' + str(Total_Confirmed) + ''']</p>
+      <p style='text-align: center; font-size: 35px; font-weight: bold; color: red'>''' + str(Total_Confirmed) + '''</p>
     
     <div class="col-sm-4" style='background-color: #fff; border-radius: 5px'>
       <p style='text-align:center; font-weight: 400 ; color: #000'>Total Deaths</p>
@@ -118,13 +116,13 @@ st.markdown('''
       <p style='text-align: center; font-size: 35px; font-weight: bold; color: #e73631'>''' + str(Total_Death) + '''</p>
     
     <div class="col-sm-4">
-      <p style='text-align: margin-right; background-color: #fff; font-weight: 400 ;color: #000'>Total Recovered</p>
-      <p style='text-align: margin-right; font-size: 15px; color: #70a82c'>[''' + str(Total_Recovered) + ''']</p>
-      <p style='text-align: margin-right ; font-size: 35px; font-weight: bold; color: #70a82c'>''' + str(Total_Recovered) + '''</p>
+      <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: #000'>Total Recovered</p>
+      <p style='text-align: center; font-size: 15px; color: #70a82c'>[''' + str(Total_Recovered) + ''']</p>
+      <p style='text-align: center ; font-size: 35px; font-weight: bold; color: #70a82c'>''' + str(Total_Recovered) + '''</p>
     </div>
     </div>
     </div>
-  
+   </div>
  ''', unsafe_allow_html=True);
     
 def get_table():
