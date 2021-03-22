19aaff6a349eb05c18573bd3c0f3563bc2ce9a3a
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-03-22 11:28:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -154,33 +154,7 @@ html,body{
 
 </body>
 ''', unsafe_allow_html=True);
-st.markdown('''
-<h1></h1>
-<div class="jumbotron text-center" style='padding: 0px';background-color:#fff>
- <div class="row" style="background-color:#fff;width:100%;margin:auto;">
-    <div class="row-sm-4">
-      <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: red'>Total Confirmed</p>
-      <p style='text-align: center; font-size: 15px; color: red'>[''' + str(Total_Confirmed) + ''']</p>
-      <p style='text-align: center; font-size: 35px; font-weight: bold; color: red'>''' + str(Total_Confirmed) + '''</p>
-    </div>
-    <div class="row-sm-4" style='background-color: #fff; border-radius: 5px'>
-      <p style='text-align:center; font-weight: 400 ; color: #000'>Total Deaths</p>
-      <p style='text-align: center; font-size: 15px; color: #e73631'>[''' + str(Total_Death) + ''']</p>
-      <p style='text-align: center; font-size: 35px; font-weight: bold; color: #e73631'>''' + str(Total_Death) + '''</p>
-    </div>
-    <div class="row-sm-4">
-      <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: #000'>Total Recovered</p>
-      <p style='text-align: center; font-size: 15px; color: #70a82c'>[''' + str(Total_Recovered) + ''']</p>
-      <p style='text-align: center ; font-size: 35px; font-weight: bold; color: #70a82c'>''' + str(Total_Recovered) + '''</p>
-     </div>
-     <div class="row-sm-4">
-      <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: #000'>Total Active</p>
-      <p style='text-align: center; font-size: 15px; color: #70a82c'>[''' + str(Total_Active) + ''']</p>
-      <p style='text-align: center ; font-size: 35px; font-weight: bold; color: #70a82c'>''' + str(Total_Active) + '''</p>
-     </div>
-  </div>
-</div>
- ''', unsafe_allow_html=True);
+
 if visualization=='Bar Chart':
     total_graph = px.bar(total, x='Status',y='Number of cases',
                                labels={'Number of cases':'Number of Total cases'},color='Status')
