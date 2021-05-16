36dc28b4fe6b185e787facd6410504ebc80bef4b
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-05-16 10:14:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -310,7 +310,7 @@ st.markdown('''
 
 #st.markdown("## **District level analysis**")
 dis1=pd.read_csv("https://api.covid19india.org/csv/latest/district_wise.csv")
-dis2=dis1[4:776]
+dis2=dis1[2:776]
 dis3=dis2.drop(607)
 dis=dis3.drop([769,773])
 #dis=pd.merge(dis,new)
