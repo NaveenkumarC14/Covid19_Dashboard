bfca24ed800daa8a8954b70830156dba457df6ad
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-05-15 22:29:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def special_format(n):
 st.markdown('''
 <div class="jumbotron text-center" style='background-color: #fff'>
  
-   <h1 style="margin: auto;color: green; width: 100%;">''' + str(special_format(b)) + ''' Vaccine Doses Administered</h1>
+   <h1 style="margin: auto;color: green; width: 100%;">''' + str(special_format(total)) + ''' Vaccine Doses Administered</h1>
 <h2></h2>
 </div>
 ''', unsafe_allow_html=True);
