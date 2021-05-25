72846e77611618e7e1b673ab7086c15805377df6
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-05-25 10:06:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ if Daily=='Tested':
 	fig=px.bar(daily_test,x='Tested As Of',y="Total Samples Tested",labels={'Tested As Of':'Date'})
 	st.plotly_chart(fig)
 if Daily=='Vaccine':
-	daily_state = st.sidebar.selectbox('Select state',daily_vaccine['State'].unique())
+	daily_state = st.sidebar.selectbox('Choose a state',daily_vaccine['State'].unique())
         selected_state_daily = daily_vaccine[daily_vaccine['State']==daily_state]
 	fig=px.bar(selected_state_daily,x='Updated On',y="Total Doses Administered",labels={'Updated On':'Date'})
 	st.plotly_chart(fig)
