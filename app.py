d6d1116261b697eba4f3fce41acfb7d5d91f9f8a
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-05-26 10:23:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -363,12 +363,15 @@ dis=dis3.drop([768,772,769,767,770,771,766,158,211,219,270,271,312,347,459,472])
 #dis=pd.merge(dis,new)
 state_select1 = st.selectbox('Select a state',dis['State'].unique())
 selected_state1 = new[new['State'] == state_select1]
-if selected_state1=="Delhi":
-	print('District ')
+
                  
     
 vaccine=selected_state1.groupby(['District'],as_index=False).agg('sum')
 vaccine_state=pd.merge(dis,vaccine)
+if selected_state1=="Delhi":
+	del=vaccine_state[vaccine_state['State']=='Delhi']
+	del.drop()
+	
 def get_table():
     datatable = vaccine_state[['State','District', 'Confirmed', 'Active', 'Recovered','Deceased','Vaccine']]
     return datatable
