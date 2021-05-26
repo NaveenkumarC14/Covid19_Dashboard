d4ecd6ffa2b48da9abfbcad13afc4e563fe022a0
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-05-26 10:27:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -368,8 +368,7 @@ selected_state1 = new[new['State'] == state_select1]
     
 vaccine=selected_state1.groupby(['District'],as_index=False).agg('sum')
 vaccine_state=pd.merge(dis,vaccine)
-if selected_state1==Delhi:
-	vaccine_state.drop(vaccine_state[vaccine_state['State']=='Delhi'])
+vaccine_state.drop(vaccine_state[vaccine_state['State']=='Delhi'])
 	
 	
 def get_table():
