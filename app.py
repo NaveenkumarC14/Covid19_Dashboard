bf6d55ae8bbcee8c4d5ee787befe0aaf665fcdb1
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-30 09:47:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ st.markdown('''
  ''', unsafe_allow_html=True);
 
 st.markdown('''
-            <p><h1>Dialy Renewable Energy Generation - Monitoring Dashboard</h1></p>
+           
             <div style="height:150px;width: 30%;background-color: #d1f0a2; float:left; left: 0px; border-radius: 2px;">
                     <div>
                         <div style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; font-size: 30px; padding: 20px 0px 0px 25px;">'''+ str(Total_Confirmed)+''' <span style="font-size: 15px; font-weight: normal"> </div>
@@ -208,9 +208,9 @@ st.markdown('''
 
             <div style="height:150px;width: 20%; background-color: white; float:left; left: 700px; border-radius: 2px; border: 2px solid #d9d9d9; border-right: None;">
                     <div>
-                        <div style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; font-size: 30px; padding: 10px 0px 0px 25px;"> <span style="font-size: 15px; font-weight: normal">MU</span> </div>
-                        <div style="padding: 0px 0px 0px 25px;">Solar<br>generation</br></div>                        
-                        <div style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; color: #33adff; font-size: 12px; padding: 5px 0px 0px 25px;"></div>
+                        <div style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; font-size: 30px; padding: 10px 0px 0px 25px;"> '''+ str(Total_Death)+'''<span style="font-size: 15px; font-weight: normal"></span> </div>
+                        <div style="padding: 0px 0px 0px 25px;"></div>                        
+                        <div style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; color: #33adff; font-size: 12px; padding: 5px 0px 0px 25px;">Death</div>
                     </div>
             </div>
 
