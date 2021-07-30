6b7c3204485c154f25da4b340f89cddeb1323587
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-30 09:40:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -207,11 +207,10 @@ st.markdown('''
 
             <div style="height:150px;width: 20%; background-color: white; float:left; left: 700px; border-radius: 2px; border: 2px solid #d9d9d9; border-right: None;">
                     <div>
-                        <div style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; font-size: 30px; padding: 10px 0px 0px 25px;">'''+ str(Total_Death) +''' <span style="font-size: 15px; font-weight: normal"></span> </div>
-                        <div style="padding: 0px 0px 0px 25px;">Total Death</div>                        
-                       
-            </div>
-
+                        <div style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; font-size: 30px; padding: 20px 0px 0px 25px;">'''+str(Total_Deaths)+''' <span style="font-size: 15px; font-weight: normal"></span> </div>
+                        <div style="padding: 0px 0px 0px 25px;"> Total Deaths</div>                        
+                        <div style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; color: #33adff; font-size: 12px; padding: 5px 0px 0px 25px;"></div>
+                    </div>
             
 
             <div style="height:150px;width: 2%; background-color: white; float:left;">
