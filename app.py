a0e175be293cf195815e03de6648ae364b00f5ca
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-30 12:39:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ st.markdown('''
             <div style="height:150px;width: 2%; background-color: white; float:left;left: 1500px; border-radius: 2px;"">
             </div>
                     <div>
-		     <div style="height:100px;width: 22%; background-color: red; float:left; left: 1700px; border-radius: 20px; border: 2px solid #d9d9d9; border-right: right;">
+		     <div style="height:100px;width: 22%; background-color: white; float:left; left: 1700px; border-radius: 20px; border: 2px solid #d9d9d9; border-right: right;">
 		    <div style="font-family: Arial, Helvetica, sans-serif;text-align: center; font-weight: bold; color: #33adff; font-size: 12px; padding: 20px 25px 0px 20px;">Confirmed</div>
                         <div style="font-family: Arial, Helvetica, sans-serif;text-align: center; font-weight: bold; font-size: 30px; padding: 10px 0px 0px 2px;">'''+ str(Total_Confirmed) +''' </div>
             </div>
@@ -178,7 +178,7 @@ st.markdown('''
             <div style="height:150px;width: 2%;background-color: white; float:left; left: 200px; border-radius: 2px;">
 	     </div>
                     <div>
-			<div style="height:100px;width: 22%; background-color: blue; float:left; left: 1500px; border-radius: 20px; border: 2px solid #d9d9d9; border-right: right;">
+			<div style="height:100px;width: 22%; background-color: white; float:left; left: 1500px; border-radius: 20px; border: 2px solid #d9d9d9; border-right: right;">
 		        <div style="font-family: Arial, Helvetica, sans-serif;text-align: center ; font-weight: bold; color: #33adff; font-size: 12px; padding: 20px 25px 0px 20px;">Active</div>
                        <div style="font-family: Arial, Helvetica, sans-serif; text-align: center ;font-weight: bold; font-size: 30px; padding: 10px 0px 0px 2px;">'''+ str(Total_Active)+'''  </div>
 	    </div>
@@ -194,7 +194,7 @@ st.markdown('''
 		<div style="height:150px;width: 2%; background-color: white; float:left;left: 300px; border-radius: 2px;"">
             </div>
                     <div>
-		    <div style="height:100px;width: 22%; background-color: green; float:left; left: 1400px; border-radius: 20px; border: 2px solid #d9d9d9; border-right: right;">
+		    <div style="height:100px;width: 22%; background-color: #90EE90; float:left; left: 1400px; border-radius: 20px; border: 2px solid #d9d9d9; border-right: right;">
 		    <div style="font-family: Arial, Helvetica, sans-serif;text-align: center; font-weight: bold; color: #33adff; font-size: 12px; padding: 20px 25px 0px 20px;">Recovered</div>
                         <div style="font-family: Arial, Helvetica, sans-serif;text-align: center; font-weight: bold; font-size: 30px; padding: 10px 0px 0px 2px;">'''+ str(Total_Recovered) +''' </div>
                   </div>
