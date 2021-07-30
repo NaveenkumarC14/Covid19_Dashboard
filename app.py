9ff7161253da5cc846936bd89ba3933601d5e7d6
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-30 12:28:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -395,7 +395,7 @@ vaccine=selected_state1.groupby(['District'],as_index=False).agg('sum')
 
 	
 def get_table():
-    datatable = state_select1[['District', 'Confirmed', 'Active', 'Recovered','Deceased']]
+    datatable = selected_state1[['District', 'Confirmed', 'Active', 'Recovered','Deceased']]
     return datatable
 datatable = get_table()
 st.dataframe(datatable)
