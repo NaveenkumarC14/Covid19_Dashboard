47fa5218e62cc9e58e72c115116207d20cef8e13
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-30 11:37:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -197,10 +197,13 @@ st.markdown('''
 st.markdown('''
            
             <div style="height:100px;width: 20%;background-color: white; float:left; left: 0px; border-radius: 2px;">
+	     </div>
+	     <div style="height:100px;width: 20%; background-color: white; float:left; left: 0px; border-radius: 2px; border: 2px solid #d9d9d9; border-right: right;">
                     <div>
 		        <div style="font-family: Arial, Helvetica, sans-serif;text-align: center ; font-weight: bold; color: #33adff; font-size: 12px; padding: 20px 25px 0px 20px;">Confirmed</div>
                        <div style="font-family: Arial, Helvetica, sans-serif; text-align: center ;font-weight: bold; font-size: 30px; padding: 10px 0px 0px 2px;">'''+ str(Total_Confirmed)+'''  </div>
-            </div
+            </div>
+	    </div>
 		<div style="height:150px;width: 2%; background-color: white; float:left;left: 150px; border-radius: 2px;"">
             </div>
             <div style="height:100px;width: 20%; background-color: white; float:left; left: 700px; border-radius: 2px; border: 2px solid #d9d9d9; border-right: right;">
