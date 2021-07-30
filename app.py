210db47294034817591f1c6aa424f9c3a83e36e6
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-30 10:16:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -197,8 +197,8 @@ st.markdown('''
            
             <div style="height:100px;width: 30%;background-color: #d1f0a2; float:left; left: 0px; border-radius: 2px;">
                     <div>
-                        <div style="font-family: Arial, Helvetica, sans-serif; text-align: center;font-weight: bold; font-size: 30px; padding: 20px 0px 0px 4px;">'''+ str(Total_Confirmed)+'''  </div>
-                     <div style="font-family: Arial, Helvetica, sans-serif;text-align: center; font-weight: bold; color: #33adff; font-size: 12px; padding: 4px 20px 0px 20px;">Confirmed</div>
+		        <div style="font-family: Arial, Helvetica, sans-serif;text-align: center; font-weight: bold; color: #33adff; font-size: 12px; padding: 4px 20px 0px 20px;">Confirmed</div>
+                       <div style="font-family: Arial, Helvetica, sans-serif; text-align: center;font-weight: bold; font-size: 30px; padding: 20px 0px 0px 2px;">'''+ str(Total_Confirmed)+'''  </div>
             </div>
                 
     ''' , unsafe_allow_html=True);
