c5a1b80e909ac01222f9e548dde5ea1b61391ba8
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-30 11:20:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ st.markdown('''
       <p style='text-align: center ; font-size: 40px; font-weight: 600; color: green'>''' + str(Total_Recovered) + '''</p>    
      </div>     
     <div class="row-sm-3">    
-      <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: blue'>Total Active</p>
+      <div style="font-family: Arial, Helvetica, sans-serif;text-align: center ; font-weight: bold; color: #33adff; font-size: 12px; padding: 20px 25px 0px 20px;">Confirmed</div>
       <p style='text-align: center; font-size: 15px; color: green'></p>
       <div style="font-family: Arial, Helvetica, sans-serif; text-align: center ;font-weight: bold; font-size: 30px; padding: 10px 0px 0px 2px;">'''+ str(Total_Confirmed)+'''  </div>
     </div> 
